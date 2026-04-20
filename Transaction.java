791ddc402dbe4f9cd203fe964ac766b3654efb5d
s156056-png <s156056@student.squ.edu.om>
2026-04-20 21:25:07 +0400

    Update Transaction.java

--- Transaction.java
+++ Transaction.java
@@ -3,22 +3,23 @@ public class Transaction {
     String merchant;
     double amount;
     String category;
+    String date;          
 
-    //Class constructor
-
-    public Transaction(int id, String merchant, double amount,String category){
+    //Class constructor (updated to include date)
+    public Transaction(int id, String merchant, double amount, String category, String date){
         this.id = id;
         this.merchant = merchant;
         this.amount = amount;
         this.category = category;
+        this.date = date;
     }
 
     /** Display Transaction  (View All Transactions).
      @return Transaction  (View All Transactions) .
 
      */
-
     public void display(){
-        System.out.printf("ID: %-10d | Merchant: %-15s  | Amount: %-10.2f  | Category: %-10s \n",id,merchant,amount,category);
+        System.out.printf("ID: %-10d | Merchant: %-15s  | Amount: %-10.2f  | Category: %-12s | Date: %s\n",
+                          id, merchant, amount, category, date);
     }
 }
